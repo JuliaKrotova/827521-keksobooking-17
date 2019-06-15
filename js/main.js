f739c0669a3c781d7af038d58b9b8564cb8b5457
JuliaKrotova <julia9867@mail.ru>
2019-06-16 00:42:19 +0700

    Доработка

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 'use strict';
 
-var TYPES = ['palace', 'flat', 'house ', 'bungalo'];
-var MAP_PIN_COUNT = 8;
-var LOCATION_X_MIN = 0;
-var LOCATION_X_MAX = 1200;
-var LOCATION_Y_MIN = 130;
-var LOCATION_Y_MAX = 630;
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 
@@ -14,16 +8,22 @@ var pinTemplate = document.querySelector('#pin')
     .content
     .querySelector('.map__pin');
 
-var getRandomElement = function (array) {
-  return array[Math.round(Math.random() * (array.length - 1))];
-};
-
-var getRandomNumber = function (min, max) {
-  var randomNumber = Math.random() * (max - min) + min;
-  return randomNumber;
-};
-
 var getMapPin = function (i) {
+  var LOCATION_X_MIN = 0;
+  var LOCATION_X_MAX = 1200;
+  var LOCATION_Y_MIN = 130;
+  var LOCATION_Y_MAX = 630;
+  var TYPES = ['palace', 'flat', 'house ', 'bungalo'];
+
+  var getRandomElement = function (array) {
+    return array[Math.round(Math.random() * (array.length - 1))];
+  };
+
+  var getRandomNumber = function (min, max) {
+    var randomNumber = Math.random() * (max - min) + min;
+    return randomNumber;
+  };
+
   var mapPin = {
     author: {
       avatar: 'img/avatars/user' + '0' + i + '.png'
@@ -32,32 +32,33 @@ var getMapPin = function (i) {
       type: getRandomElement(TYPES)
     },
     location: {
-      x: getRandomNumber(LOCATION_X_MIN, LOCATION_X_MAX) - PIN_WIDTH / 2,
-      y: getRandomNumber(LOCATION_Y_MIN, LOCATION_Y_MAX) - PIN_HEIGHT
+      x: getRandomNumber(LOCATION_X_MIN, LOCATION_X_MAX),
+      y: getRandomNumber(LOCATION_Y_MIN, LOCATION_Y_MAX)
     }
   };
   return mapPin;
 };
 
-var getMapPins = function (mapPinCount) {
+var getMapPins = function () {
   var mapPins = [];
-  for (var i = 0; i < mapPinCount; i++) {
+  for (var i = 0; i < 8; i++) {
     mapPins.push(getMapPin(i + 1));
   }
   return mapPins;
 };
 
-var showActiveMap = function (map) {
-  var element = document.querySelector(map);
+var showActiveMap = function () {
+  var element = document.querySelector('.map');
   element.classList.remove('map--faded');
 };
 
 var renderMapPin = function (mapPin) {
   var mapPinElement = pinTemplate.cloneNode(true);
-  mapPinElement.style.left = mapPin.location.x + 'px';
-  mapPinElement.style.top = mapPin.location.y + 'px';
-  mapPinElement.querySelector('img').src = mapPin.author.avatar;
-  mapPinElement.querySelector('img').alt = mapPin.offer.type;
+  var pinElement = mapPinElement.querySelector('img');
+  mapPinElement.style.left = mapPin.location.x - PIN_WIDTH / 2 + 'px';
+  mapPinElement.style.top = mapPin.location.y - PIN_HEIGHT + 'px';
+  pinElement.src = mapPin.author.avatar;
+  pinElement.alt = mapPin.offer.type;
   return mapPinElement;
 };
 
@@ -69,6 +70,6 @@ var renderMapPins = function (mapPins) {
   mapPinsList.appendChild(fragment);
 };
 
-showActiveMap('.map');
-var mapPins = getMapPins(MAP_PIN_COUNT);
+showActiveMap();
+var mapPins = getMapPins();
 renderMapPins(mapPins);
