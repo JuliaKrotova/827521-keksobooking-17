e0645ea683979bb8c20535bbdeb9a6d4fcdeaaad
JuliaKrotova <julia9867@mail.ru>
2019-06-24 12:15:34 +0700

    Реализовано перемещение маркера

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 var PIN_MAIN_WIDTH = 32;
 var PIN_MAIN_HEIGHT = 32;
-var locationPinMainX = 570;
-var locationPinMainY = 375;
+var PIN_MAIN_MIN_X = 0;
+var PIN_MAIN_MAX_X = 1200;
+var PIN_MAIN_MIN_Y = 130;
+var PIN_MAIN_MAX_Y = 630;
 
 var mapPinsListElement = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin')
@@ -23,6 +25,7 @@ var priceElement = document.querySelector('#price');
 var typeAccommodationElement = document.querySelector('#type');
 var checkInTimeElement = document.querySelector('#timein');
 var checkOutTimeElement = document.querySelector('#timeout');
+
 
 var showActiveForm = function () {
   formElement.classList.remove('ad-form--disabled');
@@ -109,17 +112,72 @@ var showActiveState = function () {
   renderMapPins(mapPins);
 };
 
-var setAddress = function () {
-  addressElement.value = (locationPinMainX + PIN_MAIN_WIDTH) + ', ' + (locationPinMainY + PIN_MAIN_HEIGHT);
+var setAddress = function (endCoords) {
+  addressElement.value = (endCoords.x + PIN_MAIN_WIDTH) + ', ' + (endCoords.y + PIN_MAIN_HEIGHT);
 };
 
-mapPinMainElement.addEventListener('click', function () {
-  showActiveState();
-});
+var onMouseDown = function (evt) {
+  evt.preventDefault();
 
-mapPinMainElement.addEventListener('mouseup', function () {
-  setAddress();
-});
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    var endCoords = {
+      x: (mapPinMainElement.offsetLeft - shift.x),
+      y: (mapPinMainElement.offsetTop - shift.y)
+    };
+
+    if (endCoords.y >= PIN_MAIN_MIN_Y && endCoords.y <= PIN_MAIN_MAX_Y) {
+      mapPinMainElement.style.top = endCoords.y + 'px';
+    }
+    if (endCoords.x >= PIN_MAIN_MIN_X - PIN_MAIN_WIDTH / 2 && endCoords.x <= PIN_MAIN_MAX_X - PIN_MAIN_WIDTH) {
+      mapPinMainElement.style.left = endCoords.x + 'px';
+    }
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    showActiveState();
+
+    var shift = {
+      x: startCoords.x - upEvt.clientX,
+      y: startCoords.y - upEvt.clientY
+    };
+
+    var endCoords = {
+      x: (mapPinMainElement.offsetLeft - shift.x),
+      y: (mapPinMainElement.offsetTop - shift.y)
+    };
+
+    setAddress(endCoords);
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+    mapPinMainElement.removeEventListener('mousedown', onMouseDown);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+};
+
+mapPinMainElement.addEventListener('mousedown', onMouseDown);
+
 
 typeAccommodationElement.addEventListener('change', function () {
   setPrice(typeAccommodationElement.value);
